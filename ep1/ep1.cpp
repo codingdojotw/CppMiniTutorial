dab961768ecc94288daad2db201f59ca71349b18
Sam Tsai <codingdojotw@gmail.com>
2013-11-08 18:08:52 +0800

    Add two tests to demo 'override' specifier.

--- ep1/ep1.cpp
+++ ep1/ep1.cpp
@@ -2,7 +2,36 @@
 //
 
 #include "stdafx.h"
+#include <memory>
 #include <gtest\gtest.h>
+
+using namespace std;
+
+struct Foo {
+	virtual int Plus(int left, int right)
+	{
+		return 2;
+	}
+};
+
+struct Bar : public Foo {
+	virtual int Plus(int left, short right)
+	{
+		return 4;
+	}
+};
+
+TEST(Episode1, OverrideTest_CallParent)
+{
+	unique_ptr<Foo> op(new Foo());
+	EXPECT_EQ(2, op->Plus(1, 1));
+}
+
+TEST(Episode1, OverrideTest_CallDerived)
+{
+	unique_ptr<Foo> op(new Bar());
+	EXPECT_EQ(4, op->Plus(1, 1));
+}
 
 
 int _tmain(int argc, _TCHAR* argv[])
